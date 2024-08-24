fc24adbb0800d09a7e805991a163a1ee8573caf9
Omar Kamal <64711605+omarkamal8@users.noreply.github.com>
2024-08-25 01:48:14 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import Root from "./routes/root";
 import Home from "./routes/home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-//Testss
+//Testssbvjuhvbjbjhy
 const router = createBrowserRouter([
   {
     path: "/",
